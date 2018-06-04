f715fed3fff4f87b452da0a6655ed1e94cf35bbb
Rahuly2k <rahuly2k98@gmail.com>
2018-06-04 13:09:09 +0530

    [TASK] Handle sum of zero to multiples numbers

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -32,14 +32,39 @@ class CalculatorTest extends TestCase
     }
 
     /**
-     * Data provider for sum from 0 to 2 numers
+     * Test sum with o to multple numbers
+     *
+     * @dataProvider dataForSumWithMultipleNumbers
+     */
+    public function testSumtZeroToMultipleNumbers($data, $sum)
+    {
+        $this->assertEquals($sum, $this->calculator->add($data));
+    }
+
+    /**
+     * Data provider for sum from 0 to 2 numbers
      */
     public function dataForSumFromZeroToTwoNumbers()
     {
-        return array(
+        return [
             ['', 0],
             ['1', 1],
             ['2,3', 5]
-        );
+        ];
+    }
+
+    /**
+     * Data provider for sum with multiple numbers
+     */
+    public function dataForSumWithMultipleNumbers()
+    {
+        return [
+            ['', 0],
+            ['1', 1],
+            ['2,3', 5],
+            ['4,5,6', 15],
+            ['2,3,4,5', 14],
+            ['4,7,3,4,7,3,5,6,7,4,3,2,5,7,5,3,4,6,7,8,9,5,5,5,4,3,2', 133]
+        ];
     }
 }
